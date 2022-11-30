b83df55f6c884ccb0275688e9e84442d169b9cba
birdsoftheworld <birdsoftheworld@gmail.com>
2022-11-29 18:31:57 -0800

    feat(testing): intake calibration

--- src/main/java/frc/robot/commands/IntakeCalibration.java
+++ src/main/java/frc/robot/commands/IntakeCalibration.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.subsystems.Intake;
@@ -8,25 +9,41 @@ import java.util.Set;
 
 public class IntakeCalibration extends CommandBase {
     private final Intake intake;
+    private final Direction direction;
 
-    public IntakeCalibration(Intake intake) {
+    public IntakeCalibration(Intake intake, Direction direction) {
         this.intake = intake;
+        this.direction = direction;
     }
 
     @Override
     public void initialize() {
-        intake.setTargetAngle(0);
-        intake.goDown();
+        intake.setIdleMode(CANSparkMax.IdleMode.kBrake);
+        intake.setTargetAngle(intake.getEncoderPositionDegrees() + (5 * direction.mult));
+        intake.setPosition(Intake.Position.Match);
     }
 
     @Override
     public void execute() {
-        intake.setTargetAngle(intake.getTargetAngle() + 5f);
-        intake.goUp();
     }
 
     @Override
     public Set<Subsystem> getRequirements() {
         return Set.of(intake);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+
+    public enum Direction {
+        Up(1), Down(-1);
+
+        public final double mult;
+
+        Direction(double mult) {
+            this.mult = mult;
+        }
     }
 }
