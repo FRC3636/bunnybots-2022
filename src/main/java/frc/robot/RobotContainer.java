e62b88919eb64039f4f1c0a4e73c3c25935852c9
Gavin-Niederman <gavinniederman@gmail.com>
2022-11-30 20:16:31 -0500

    change Sparks to SparkMaxs

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,10 +19,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.DriveConfig.DriveScheme;
 import frc.robot.commands.DriveCommand;
-import frc.robot.commands.IntakeCalibration;
-import frc.robot.commands.IntakeMotorOnlyCommand;
 import frc.robot.subsystems.DriveTrain;
-import frc.robot.subsystems.Intake;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -36,7 +33,6 @@ import frc.robot.subsystems.Intake;
 public class RobotContainer {
     // The robot's subsystems and commands are defined here...
     private final DriveTrain driveTrain = new DriveTrain();
-    private final Intake intake = new Intake();
     public static final Joystick joystickL = new Joystick(Constants.JOYSTICK_LEFT_PORT);
     public static final Joystick joystickR = new Joystick(Constants.JOYSTICK_RIGHT_PORT);
     public static final SendableChooser<String> drivePresetsChooser = new SendableChooser<String>();
@@ -84,10 +80,6 @@ public class RobotContainer {
      */
     private void configureButtonBindings() {
 //        driveTrain.setDefaultCommand(new DriveCommand(driveTrain));
-        new JoystickButton(joystickL, 3).whenPressed(new IntakeCalibration(intake, IntakeCalibration.Direction.Up));
-        new JoystickButton(joystickL, 2).whenPressed(new IntakeCalibration(intake, IntakeCalibration.Direction.Down));
-        new JoystickButton(joystickL, 4).whileHeld(new IntakeMotorOnlyCommand(intake, IntakeMotorOnlyCommand.Direction.In));
-        new JoystickButton(joystickL, 5).whileHeld(new IntakeMotorOnlyCommand(intake, IntakeMotorOnlyCommand.Direction.Out));
     }
 
     /**
--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -3,9 +3,9 @@ package frc.robot.subsystems;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
@@ -14,39 +14,30 @@ public class DriveTrain implements Subsystem {
     // these fields represent the motor hardware
     // they are declared private because they should
     // never be accessed directly outside this subsystem
-    private final Spark leftMotor = new Spark(Constants.DriveTrain.LEFT_MOTOR_PORT);
-    private final Spark rightMotor = new Spark(Constants.DriveTrain.RIGHT_MOTOR_PORT);
-    private final DifferentialDrive robotDrive = new DifferentialDrive(leftMotor, rightMotor);
-
-
-    private final Encoder leftEncoder = new Encoder(
-            Constants.DriveTrain.LEFT_ENCODER_PORT_A,
-            Constants.DriveTrain.LEFT_ENCODER_PORT_B,
-            true
-    );
-    private final Encoder rightEncoder = new Encoder(
-            Constants.DriveTrain.RIGHT_ENCODER_PORT_A,
-            Constants.DriveTrain.RIGHT_ENCODER_PORT_B,
-            false
-    );
+    private final CANSparkMax rightMotor1 = new CANSparkMax(1, MotorType.kBrushed); // has encoder
+    private final CANSparkMax rightMotor2 = new CANSparkMax(2, MotorType.kBrushed);
+    private final CANSparkMax leftMotor1 = new CANSparkMax(4, MotorType.kBrushed); // has encoder
+    private final CANSparkMax leftMotor2 = new CANSparkMax(3, MotorType.kBrushed);
+    private final DifferentialDrive robotDrive = new DifferentialDrive(rightMotor1, leftMotor1);
 
     private final DifferentialDriveOdometry odometry = new DifferentialDriveOdometry(
             new Rotation2d()
     );
 
     public DriveTrain() {
-        leftMotor.setInverted(true);
+        rightMotor2.follow(rightMotor1);
+        leftMotor2.follow(leftMotor1);
+        leftMotor1.setInverted(true);
 
-        leftEncoder.setDistancePerPulse(Constants.DriveTrain.WHEEL_CIRCUMFERENCE / Constants.DriveTrain.PULES_PER_ROTATION);
-        rightEncoder.setDistancePerPulse(Constants.DriveTrain.WHEEL_CIRCUMFERENCE / Constants.DriveTrain.PULES_PER_ROTATION);
+        // TODO: get acutal conversion factor
+        leftMotor1.getEncoder().setPositionConversionFactor(Constants.DriveTrain.WHEEL_CIRCUMFERENCE / Constants.DriveTrain.PULES_PER_ROTATION);
+        rightMotor1.getEncoder().setPositionConversionFactor(Constants.DriveTrain.WHEEL_CIRCUMFERENCE / Constants.DriveTrain.PULES_PER_ROTATION);
     }
 
     @Override
     public void periodic() {
-        double dl = leftEncoder.getDistance();
-        double dr = rightEncoder.getDistance();
-        System.out.println(leftEncoder.getRaw());
-        System.out.println(rightEncoder.getRaw());
+        double dl = leftMotor1.getEncoder().getPosition();
+        double dr = rightMotor1.getEncoder().getPosition();
         double w = Constants.DriveTrain.TRACK_WIDTH;
         Rotation2d rotation = new Rotation2d((dr - dl) / w);
         odometry.update(rotation, dl, dr);
@@ -61,8 +52,8 @@ public class DriveTrain implements Subsystem {
     }
 
     private void resetEncoders() {
-        leftEncoder.reset();
-        rightEncoder.reset();
+        leftMotor1.getEncoder().setPosition(0);
+        rightMotor1.getEncoder().setPosition(0);
     }
 
     public void arcadeDrive(double speed, double rotation) {
