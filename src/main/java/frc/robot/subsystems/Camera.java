9151aa19348863c85e568cf827f133740019acd2
Lewis McClelland <mcclelll@icloud.com>
2022-12-13 17:08:37 -0800

    feat: add camera subsystem

---
+++ src/main/java/frc/robot/subsystems/Camera.java
@@ -0,0 +1,63 @@
+package frc.robot.subsystems;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Camera extends SubsystemBase {
+    private final PhotonCamera camera;
+
+    /**
+     * @param address - The hostname of the camera, usually {@code photonvision}.
+     */
+    public Camera(String address) {
+        camera = new PhotonCamera(address);
+        camera.setDriverMode(false);
+    }
+
+    public List<PhotonTrackedTarget> findTargets() {
+        PhotonPipelineResult snapshot = camera.getLatestResult();
+        return snapshot.getTargets();
+    }
+
+    private Optional<Transform3d> target = Optional.empty();
+
+    @Override
+    public void periodic() {
+        PhotonPipelineResult result = camera.getLatestResult();
+        if (result.hasTargets()) {
+            target = Optional.of(result.getBestTarget().getBestCameraToTarget());
+        } else {
+            target = Optional.empty();
+        }
+    }
+
+    /** Will return an empty value when the target is not visible */
+    public Optional<Transform3d> getTargetRelative() {
+        return target;
+    }
+
+    /** Will return an empty value when the target is not visible */
+    public Optional<Transform2d> getTargetRelative2D() {
+        return target.map(target -> {
+            var translation = target.getTranslation().toTranslation2d();
+            var rotation = target.getRotation().toRotation2d();
+            return new Transform2d(translation, rotation);
+        });
+    }
+
+    public static Pose2d calculateRobotPositionOnField(Pose2d targetLocationOnField,
+            Transform2d targetRelativeToRobot) {
+        return targetLocationOnField.transformBy(targetRelativeToRobot.inverse());
+    }
+}
