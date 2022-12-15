65ecd4061c0b2bfc24e1d9bf580a9fb3d8c8fd32
Max Niederman <max@maxniederman.com>
2022-12-15 13:05:26 -0800

    feat(intake): implement positioned stop

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,17 +2,27 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.RelativeEncoder;
+
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.Constants;
 
 public class Intake implements Subsystem {
-    private final DigitalInput readSwitch = new DigitalInput(Constants.Intake.INTAKE_READ_SWITCH_PORT);
+    private final DigitalInput resetSwitch = new DigitalInput(Constants.Intake.INTAKE_RESET_SWITCH_PORT);
+    private final PIDController positionController = new PIDController(Constants.Intake.INTAKE_CONTROLLER_P, Constants.INTAKE_CONTROLLER_I, Constants.Intake.INTAKE_CONTROLLER_D);
 
     private final CANSparkMax intakeMotor = new CANSparkMax(Constants.Intake.INTAKE_MOTOR_PORT, CANSparkMaxLowLevel.MotorType.kBrushless);
+    private final RelativeEncoder intakeEncoder = intakeMotor.getEncoder();
     private State currentState = State.Stop;
+
     @Override
     public void periodic() {
+        if (resetSwitch.get()) {
+            intakeEncoder.setPosition(0);
+        }
+
         switch (currentState) {
             case Intake:
                 intakeMotor.set(Constants.Intake.INTAKE_SPEED);
@@ -21,7 +31,7 @@ public class Intake implements Subsystem {
                 intakeMotor.set(-Constants.Intake.INTAKE_SPEED);
                 break;
             case Stop:
-                intakeMotor.set(0); // FIXME Return to vertical
+                intakeMotor.set(positionController.calculate(intakeEncoder.getPosition(), Math.round(intakeEncoder.getPosition()) + Constants.Intake.INTAKE_STOP_POSITION));
                 break;
         }
     }
